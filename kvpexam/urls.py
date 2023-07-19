8d1e4d34eb5979f7cf672f8e7bde8ec0bf8e5946
V-Srivatsan <vatsan.programming@gmail.com>
2023-07-19 17:29:03 +0530

    Changed to admin

--- kvpexam/urls.py
+++ kvpexam/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('seating/', include('seating.urls')),
-    path('/', include('app.urls')),
+    path('dj-admin/', admin.site.urls),
+    path('admin/', include('admin.urls')),
+    path('', include('app.urls')),
 ]
