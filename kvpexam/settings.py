84babdfdc0e7660507a89649953b40bc69b2ef53
V-Srivatsan <vatsan.programming@gmail.com>
2023-07-23 19:11:04 +0530

    Production settings

--- kvpexam/settings.py
+++ kvpexam/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#ok**jro*c%b_azu%y5z56f(kg+z08tgx=q1kc&6@$8ap9h682'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kvpexam.pythonanywhere.com']
 
 
 # Application definition
